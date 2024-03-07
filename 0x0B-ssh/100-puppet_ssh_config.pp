69232cecbbe3e89969d84af60d8d5d594ca1310d
NiniolaX <loveafinni16@gmail.com>
2024-03-07 17:55:10 +0100

    Modified file path to  /etc/ssh/ssh_config

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # Set up client SSH configuration file so user can connect without password
-file {'~/.ssh/config':
+file {'/etc/ssh/ssh_config':
 	ensure => file,
-	content => 'Host 398697-web-01\n	User=ubuntu\n	HostName=34.224.4.177\n	IdentityFile=~/.ssh/school\n	PasswordAuthentication=no',
-	require => File[~/.ssh]
+	content => 'Host 398697-web-01\n	User ubuntu\n	HostName 34.224.4.177\n	IdentityFile ~/.ssh/school\n	PasswordAuthentication no',
+	require => File['/etc/ssh/ssh_config']
 }
