18c7bf48ba96467826b0648bacb5066a89c888ed
NiniolaX <loveafinni16@gmail.com>
2024-06-07 15:21:15 +0100

    Added puppet script

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,15 @@
+# Define file path
+$file_path = '/var/www/html/wp-settings.php'
+
+# Define string causing error
+$old_string = 'class-wp-locale.phpp'
+
+# Define correct string
+$new_string = 'class-wp-locale.php'
+
+# Perform string replacement on file
+exec {'replace_class-wp-locale.phpp':
+  command => "sudo sed -i 's/${old_string}/${new_string}' ${file_path}",
+  path    => '/bin:/usr/bin',
+  onlyif  => "grep -q '${old_string}' ${file_path}"
+}
